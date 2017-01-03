6459088d8e769b5515481bd2b76b3eb7bc120779
Leonard Seffer <leonardseffer@gmail.com>
2017-01-03 21:07:28 +0200

    modified setup.py to include .txt files and added the .txt files containing the listed companies

--- stock_screener/setup.py
+++ stock_screener/setup.py
@@ -4,4 +4,9 @@ setup(
     name="stock_screener",
     version="1.0",
     packages=find_packages(),
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt'],
+    },
 )
